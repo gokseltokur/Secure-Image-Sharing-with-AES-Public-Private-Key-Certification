022d96da51926099537a1734e60362c94266bb6a
ertugrulsagdic <38252389+ertugrulsagdic@users.noreply.github.com>
2021-06-18 17:59:53 +0300

    file save

--- client/client.py
+++ client/client.py
@@ -7,53 +7,59 @@ import logging
 import tqdm
 import os
 
+
 def create_public_private_key():
-	private_key = rsa.generate_private_key(
+    private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048,
         backend=default_backend()
     )
 
-	public_key = private_key.public_key()
+    public_key = private_key.public_key()
 
-	store_private_key(private_key)
-	store_public_key(public_key)
+    store_private_key(private_key)
+    store_public_key(public_key)
 
-	return public_key, private_key
+    return public_key, private_key
+
 
 def store_private_key(private_key):
-	pem = private_key.private_bytes(
-		encoding=serialization.Encoding.PEM,
-		format=serialization.PrivateFormat.PKCS8,
-		encryption_algorithm=serialization.NoEncryption()
-	)
+    pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
 
-	with open('private_key.pem', 'wb') as f:
-		f.write(pem)
+    with open('client/private_key.pem', 'wb') as f:
+        f.write(pem)
+
 
 def store_public_key(public_key):
-	pem = public_key.public_bytes(
-		encoding=serialization.Encoding.PEM,
-		format=serialization.PublicFormat.SubjectPublicKeyInfo
-	)
+    pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
 
-	with open('public_key.pem', 'wb') as f:
-		f.write(pem)
+    with open('client/public_key.pem', 'wb') as f:
+        f.write(pem)
+
 
 def send_file(s, filename, filesize):
-	progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-	with open(filename, "rb") as f:
-		while True:
-			# read the bytes from the file
-			bytes_read = f.read(2048)
-			if not bytes_read:
-				# file transmitting is done
-				break
-			# we use sendall to assure transimission in 
-			# busy networks
-			s.sendall(bytes_read)
-			# update the progress bar
-			progress.update(len(bytes_read))
+    progress = tqdm.tqdm(range(
+        filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "rb") as f:
+        while True:
+            # read the bytes from the file
+            bytes_read = f.read(2048)
+            if not bytes_read:
+                # file transmitting is done
+                break
+            # we use sendall to assure transimission in
+            # busy networks
+            s.sendall(bytes_read)
+            # update the progress bar
+            progress.update(len(bytes_read))
+
 
 # create an ipv4 (AF_INET) socket object using the tcp protocol (SOCK_STREAM)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -63,11 +69,11 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 1233))
 response = client.recv(2048)
 # Input UserName
-name = input(response.decode())	
+name = input(response.decode())
 client.send(str.encode(name))
 response = client.recv(2048)
 # Input Password
-password = input(response.decode())	
+password = input(response.decode())
 client.send(str.encode(password))
 ''' Response : Status of Connection :
 	1 : Registeration successful 
@@ -76,13 +82,13 @@ client.send(str.encode(password))
 '''
 # Input Public Key
 public_key, _ = create_public_private_key()
-#client.send(str.encode(public_key))
-filesize = os.path.getsize('public_key.pem')
+# client.send(str.encode(public_key))
+filesize = os.path.getsize('client/public_key.pem')
 client.send(str.encode(str(filesize)))
-send_file(client, 'public_key.pem', filesize)
+send_file(client, 'client/public_key.pem', filesize)
 
 
-# Receive response 
+# Receive response
 response = client.recv(2048)
 response = response.decode()
 
